2e1e57c70d96e4c21068c6f1bf26d6dfa22658ab
unknown <vedstandard@runbox.com>
2023-09-29 20:57:51 +0200

    main branch - empty project

---
+++ src/test/java/com/example/springPlayground/SpringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springPlayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
