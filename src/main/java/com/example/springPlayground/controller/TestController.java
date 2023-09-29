e08312fbbcb5fa8d8af583128d4d164ab442d1c8
unknown <vedstandard@runbox.com>
2023-09-29 21:42:53 +0200

    forgot git add

--- src/main/java/com/example/springPlayground/controller/TestController.java
+++ src/main/java/com/example/springPlayground/controller/TestController.java
@@ -1,2 +1,23 @@
-package com.example.springPlayground.controller;public class TestController {
+package com.example.springPlayground.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String test(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello2")
+    @PreAuthorize("hasRole('client_admin')")
+    public String test2(){
+        return "Hello World 2 -- ADMIN";
+    }
+
 }
